bd62d2dd58a7a8dab3362b5dc06e74ae26c25c08
MykolaDekusar <ndekusar97@gmail.com>
2024-10-26 17:43:11 +0200

    Use of default inside of the switch

--- JavaSwitch/src/Main.java
+++ JavaSwitch/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		//switch ci permette di comparare una variabile con un elenco di valori
-		String giorno="Giovedi";
+		String giorno="Ciao";
 		//dentro lo switch mettiamo la nostra variabile che vogliamo comparare
 		switch(giorno) {
 		//nel case metto un valore il quale verrà comparato alla nostra variabile e 
@@ -23,6 +23,8 @@ public class Main {
 		break;
 		case "Domenica": System.out.println("E' Domenica");
 		break;
+		//se nessuno dei casi risulta vero allora imposto un default
+		default:System.out.println("Non hai inserito un giorno della settimana");
 		}
 	}
 
