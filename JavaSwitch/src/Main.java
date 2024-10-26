c7b9e49a0f58c01e551f9153f8e9bc4cf12e49eb
MykolaDekusar <ndekusar97@gmail.com>
2024-10-26 17:39:45 +0200

    use of swtich, case and break

--- JavaSwitch/src/Main.java
+++ JavaSwitch/src/Main.java
@@ -2,8 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//switch ci permette di comparare una variabile con un elenco di valori
+		String giorno="Martedi";
+		switch(giorno) {
+		case "Lunedi":System.out.println("E' lunedi");
+		break;
+		}
 	}
 
 }
