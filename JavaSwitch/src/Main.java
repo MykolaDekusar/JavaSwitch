c3789fc20a6e9df9bf1383ff70d6e8c156005679
MykolaDekusar <ndekusar97@gmail.com>
2024-10-26 17:41:48 +0200

    Test of multiple switch cases

--- JavaSwitch/src/Main.java
+++ JavaSwitch/src/Main.java
@@ -3,9 +3,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		//switch ci permette di comparare una variabile con un elenco di valori
-		String giorno="Martedi";
+		String giorno="Giovedi";
+		//dentro lo switch mettiamo la nostra variabile che vogliamo comparare
 		switch(giorno) {
-		case "Lunedi":System.out.println("E' lunedi");
+		//nel case metto un valore il quale verrà comparato alla nostra variabile e 
+		//se sarà vero allora stamperà è lunedi
+		case "Lunedi":System.out.println("E' Lunedi");
+		//è importante usare il break per uscire dallo switch una volta che la condizione si è verificata
+		break;
+		case "Martedi": System.out.println("E' Martedi");
+		break;
+		case "Mercoledi": System.out.println("E' Mercoledi");
+		break;
+		case "Giovedi": System.out.println("E' Giovedi");
+		break;
+		case "Venerdi": System.out.println("E' Venerdi");
+		break;
+		case "Sabato": System.out.println("E' Sabato");
+		break;
+		case "Domenica": System.out.println("E' Domenica");
 		break;
 		}
 	}
